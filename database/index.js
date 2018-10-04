0c97235d1e96f3c29c8f14289bd62b539e658351
Daniel Kang <danielkang674@gmail.com>
2018-10-03 19:58:00 -0500

    Basic schema save find user add medium

--- database/index.js
+++ database/index.js
@@ -10,20 +10,100 @@ sequelize.authenticate()
   .catch(console.log);
 
 const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
+  id_token: Sequelize.STRING
 });
 
-// User.create({
-//   firstName: 'Michael',
-//   lastName: 'Thomson'
+const Medium = sequelize.define('medium', {
+  title: Sequelize.STRING,
+  creator: Sequelize.STRING,
+  type: Sequelize.STRING,
+  image: Sequelize.STRING,
+  synopsis: Sequelize.STRING,
+  moviedb_id: Sequelize.INTEGER,
+  popularity: Sequelize.DECIMAL,
+  vote_avg: Sequelize.DECIMAL,
+  vote_count: Sequelize.INTEGER
+});
+
+const User_Media = sequelize.define('user_media', {
+  rating: Sequelize.INTEGER
+});
+
+User.belongsToMany(Medium, { through: User_Media });
+Medium.belongsToMany(User, { through: User_Media });
+
+User.sync({ force: true })
+  .then(() => {
+    return Medium.sync({ force: true });
+  })
+  .then(() => {
+    return User_Media.sync({ force: true });
+  })
+  .then(() => {
+    const testmedium = Medium.build({
+      title: 'Inception',
+      creator: 'Bruce Willis',
+      type: 'TV Show',
+      image: 'www.idiehardmoveposter.com',
+      synopsis: 'Ki yay within a yippe',
+      moviedb_id: 12345,
+      popularity: 10,
+      vote_avg: 3.142,
+      vote_count: 9000
+    });
+    return testmedium.save()
+  })
+  .then((testmedium) => {
+    const testUser = User.build({
+      id_token: 'auth0|12345'
+    });
+    return testUser.save().then((testUser) => {
+      testUser.addMedium(testmedium);
+    })
+  })
+  .then(() => {
+    return User.findAll().then(data => console.log(data[0].dataValues));
+  })
+  .then(() => {
+    return Medium.findAll().then(data => console.log(data[0].dataValues));
+  })
+  .catch(console.log);
+
+const addUser = (id_token) => {
+  const testuser = User.build({ id_token });
+  return testuser.save();
+};
+
+const addMedium = (mediumObj) => {
+  const testmedium = Medium.build(mediumObj);
+  return testmedium.save();
+};
+
+const findOneUserByToken = (id_token) => {
+  const testOneUser = User.findOne({ attributes: ['id'], where: { id_token } });
+  return testOneUser;
+};
+
+module.exports = { addUser, addMedium, findOneUserByToken };
+// Medium.sync({ force: true }).then(() => {
+//   return Medium.create({
+//     title: 'Inception',
+//     creator: 'Bruce Willis',
+//     type: 'TV Show',
+//     image: 'www.diehardmoveposter.com',
+//     synopsis: 'Dreams within a dream',
+//     moviedb_id: 12345,
+//     popularity: 7.4,
+//     vote_avg: 3.4,
+//     vote_count: 9000
+//   })
+//     .catch(console.log);
+// });
+
+// User.findAll().then(data => {
+//   console.log('find all users', data[0].dataValues);
 // }).catch(console.log);
 
-User.findAll().then(data => {
-  console.log('find all', data[0].dataValues.firstName, data[1].dataValues.firstName);
-}).catch(console.log);
-
+// Medium.findAll().then(data => {
+//   console.log('find all media', data[0].dataValues);
+// }).catch(console.log);
--- index.js
+++ index.js
@@ -2,12 +2,48 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/users/signup', (req, res) => {
+  let { id_token } = req.body;
+  db.addUser(id_token)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
+app.post('/api/addMedium', (req, res) => {
+  let mediumObj = req.body;
+  db.addMedium(mediumObj)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/users/getUserByToken', (req, res) => {
+  let { id_token } = req.body;
+  db.findOneUserByToken(id_token)
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    })
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
