c038a9fdc6e89028bf47b57fbfb1e5f3b236886e
Daniel Kang <40406108+danielkang674@users.noreply.github.com>
2018-10-16 16:30:09 -0500

    Mergemaster10 (#21)
    * added genres tables, queries, and working incrementing
    
    * working on incrementing
    
    * working increment functionality
    
    * working incremntal genre relationship between user and genres. unique genres are good
    
    * Added that muthafuckin function work baby
    
    * Found missing function
    
    * Implemented getTopGenres on db and db server (#12)
    
    * Got array of books from genre
    
    * Feature/server db api (#13)
    
    * Implemented getTopGenres on db and db server
    
    * Implemented Medium to Genre
    
    * Staging final changes
    
    * Got individual books data
    
    * Add genres file
    
    * Make Book table
    
    * save 1 book change to Upsert
    
    * added medium-genre relations (#14)
    
    * added medium-genre relations
    
    * Removed token
    
    * Add 5 books to db
    
    * Add book, genre, and join table
    
    * fixed addingMediums that already existed to new users
    
    * CI/CD
    
    * Circle CI
    
    * Rebuild script tweak
    
    * Remove Docker pull
    
    * Docker pull again
    
    * Get all books by genre ID (#20)
    
    * Get all books by genre ID
    
    * Rename to genre_id

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/db/users/signup', (req, res) => {
-  let { id_token } = req.body;
+  const { id_token } = req.body;
   db.addUser(id_token)
     .then(() => {
       res.sendStatus(200);
@@ -27,8 +27,27 @@ app.post('/db/users/signup', (req, res) => {
 });
 
 app.post('/db/addMedium', (req, res) => {
-  let { mediumObj, id_token } = req.body;
+  const { mediumObj, id_token } = req.body;
+  const { genre_id, moviedb_id } = mediumObj;
   db.addMedium(mediumObj, id_token)
+    .then(() => {
+      db.addGenreToMedium(genre_id, moviedb_id)
+      // res.sendStatus(200);
+    })
+    .then(results => {
+      console.log('Made it to then catch', results);
+      res.sendStatus(200)
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/db/addGenre', (req, res) => {
+  //Send mediumObj here as well
+  const { genre_ids, id_token } = req.body;
+  db.addGenreToUser(genre_ids, id_token)
     .then(() => {
       res.sendStatus(200);
     })
@@ -39,7 +58,7 @@ app.post('/db/addMedium', (req, res) => {
 });
 
 app.post('/db/users/getUserByToken', (req, res) => {
-  let { id_token } = req.body;
+  const { id_token } = req.body;
   db.findOneUserByToken(id_token)
     .then(data => {
       res.json(data);
@@ -51,7 +70,7 @@ app.post('/db/users/getUserByToken', (req, res) => {
 });
 
 app.post('/db/getLastThreeMedia', (req, res) => {
-  let { id_token } = req.body;
+  const { id_token } = req.body;
   db.getLastThreeMedia(id_token)
     .then(data => {
       let massaged = data.map(el => {
@@ -64,6 +83,45 @@ app.post('/db/getLastThreeMedia', (req, res) => {
       console.log(err);
       res.sendStatus(500);
     })
+});
+
+app.post('/db/getTopGenres', (req, res) => {
+  const { id_token } = req.body;
+  db.getTopThreeGenres(id_token)
+    .then(data => {
+      const body = [];
+      data.forEach(result => {
+        const { genre_score, genreId } = result.dataValues;
+        body.push({ genre_score, genreId });
+      });
+      res.send(body);
+    })
+    .catch(err => {
+      console.log('----------\nError getting Top Genres\n----------\n', err);
+      res.sendStatus(500);
+    })
+});
+
+app.post('/db/addGoodReadsBook', async (req, res) => {
+  let { bookObj, genre_id, name } = req.body;
+  try {
+    await db.addBookFromScrape(bookObj, genre_id, name);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/db/getBookRecsByGenre/:genre_id', async (req, res) => {
+  const { genre_id } = req.params;
+  try {
+    let books = await db.getBooksByGenre(genre_id);
+    res.json(books);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(PORT, () => {
