a6831d651c6ed57dfee06642734ecc46d88859ec
Daniel Kang <40406108+danielkang674@users.noreply.github.com>
2018-10-16 16:36:55 -0500

    Mergemaster11 (#23)
    * added genres tables, queries, and working incrementing
    
    * working on incrementing
    
    * working increment functionality
    
    * working incremntal genre relationship between user and genres. unique genres are good
    
    * Added that muthafuckin function work baby
    
    * Found missing function
    
    * Implemented getTopGenres on db and db server (#12)
    
    * Got array of books from genre
    
    * Feature/server db api (#13)
    
    * Implemented getTopGenres on db and db server
    
    * Implemented Medium to Genre
    
    * Staging final changes
    
    * Got individual books data
    
    * Add genres file
    
    * Make Book table
    
    * save 1 book change to Upsert
    
    * added medium-genre relations (#14)
    
    * added medium-genre relations
    
    * Removed token
    
    * Add 5 books to db
    
    * Add book, genre, and join table
    
    * fixed addingMediums that already existed to new users
    
    * CI/CD
    
    * Circle CI
    
    * Rebuild script tweak
    
    * Remove Docker pull
    
    * Docker pull again
    
    * Get all books by genre ID (#20)
    
    * Get all books by genre ID
    
    * Rename to genre_id
    
    * Rebuild remove -t flag (#22)

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,4 @@ else
   echo "Restarting container.."
   docker stop teamboosted-database && docker rm -f teamboosted-database
 fi
-docker pull -t danielkang674/teamboosted-database:latest && docker run --name teamboosted-database -p 8081:8081 --network br0 --env-file .env -d danielkang674/teamboosted-database:latest
+docker pull danielkang674/teamboosted-database:latest && docker run --name teamboosted-database -p 8081:8081 --network br0 --env-file .env -d danielkang674/teamboosted-database:latest
